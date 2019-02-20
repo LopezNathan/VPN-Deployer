0c18474f0f935d887d4908c932a78762d3bcf8f9
Nathan Lopez <contact@lopeznathan.com>
2019-02-20 03:40:42 -0600

    vpn-deploy.py v0.3 - Moved user_data Value into Script

--- vpn-deploy.py
+++ vpn-deploy.py
@@ -2,10 +2,10 @@
 # VPN Deploy Script
 # Fully Install OpenVPN on DigitalOcean Automatically
 # Utilizes OpenVPN-Install by Angristan (https://github.com/Angristan/OpenVPN-install)
-# Version 0.2
+# Version 0.3
 
 import os
-import fileinput
+#import fileinput
 import argparse
 import requests
 
@@ -16,17 +16,25 @@ parser.add_argument("ip", help="Your IP Address")
 parser.add_argument("email", help="Your Email Address")
 args = parser.parse_args()
 
-with fileinput.FileInput('/tmp/user-data.txt', inplace=True, backup='.bak') as file:
-    for line in file:
-        print(line.replace('YOUR-IP', args.ip), end='')
+user_data = f"""#!/bin/bash
+export IP={args.ip}
+export EMAIL={args.email}
+yum -y update && yum -y upgrade
+yum -y install wget
+wget -O /root/openvpn-deploy.sh https://raw.githubusercontent.com/LopezNathan/VPN-Deployer/master/OpenVPN-Deploy.sh
+chmod +x /root/openvpn-deploy.sh && bash /root/openvpn-deploy.sh"""
 
-with fileinput.FileInput('/tmp/user-data.txt', inplace=True, backup='.bak') as file:
-    for line in file:
-        print(line.replace('YOUR-EMAIL', args.email), end='')
+# with fileinput.FileInput('/tmp/user-data.txt', inplace=True, backup='.bak') as file:
+#     for line in file:
+#         print(line.replace('YOUR-IP', args.ip), end='')
 
-with open('/tmp/user-data.txt', 'r') as myfile:
-    user_data=myfile.read()
-myfile.close()
+# with fileinput.FileInput('/tmp/user-data.txt', inplace=True, backup='.bak') as file:
+#     for line in file:
+#         print(line.replace('YOUR-EMAIL', args.email), end='')
+
+# with open('/tmp/user-data.txt', 'r') as myfile:
+#     user_data=myfile.read()
+# myfile.close()
     
 payload = {'name': 'test', 'region': 'nyc3', 'size': '512mb', 'image': 'centos-7-x64', 'user_data': user_data}
 create = requests.post("https://api.digitalocean.com/v2/droplets", params=payload, headers={"Authorization": "Bearer %s" % DO_API_TOKEN, "Content-Type": "application/json"})
