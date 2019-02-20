0f596838d036569d63d900b28a3e75613674852a
Nathan Lopez <contact@lopeznathan.com>
2019-02-20 17:00:06 -0600

    vpn-deploy.py v0.5 - Added Deploy Class

--- vpn-deploy.py
+++ vpn-deploy.py
@@ -2,7 +2,7 @@
 # VPN Deploy Script
 # Fully Install OpenVPN on DigitalOcean Automatically
 # Utilizes OpenVPN-Install by Angristan (https://github.com/Angristan/OpenVPN-install)
-# Version 0.4
+# Version 0.5
 
 import os
 import argparse
@@ -17,18 +17,24 @@ args = parser.parse_args()
 
 #TODO (user-data): CHANGE WGET TO CURL (installed by default?) AND REMOVE YUM (already in-use within bash script)
 
-droplet = digitalocean.Droplet(token=f"{DO_API_TOKEN}",
-                               name='test',
-                               region='nyc1',
-                               image='centos-7-x64',
-                               size_slug='512mb',
-                               user_data=f"""#!/bin/bash
-export IP={args.ip}
-export EMAIL={args.email}
-yum -y update && yum -y upgrade
-yum -y install wget
-wget -O /root/openvpn-deploy.sh https://raw.githubusercontent.com/LopezNathan/VPN-Deployer/master/OpenVPN-Deploy.sh
-chmod +x /root/openvpn-deploy.sh && bash /root/openvpn-deploy.sh""",
-                               backups=True)
+class Deploy:
 
-droplet.create()
+    def create_vpn(self, ip_address, email_address):
+        droplet = digitalocean.Droplet(token=f"{DO_API_TOKEN}",
+                                    name='test',
+                                    region='nyc1',
+                                    image='centos-7-x64',
+                                    size_slug='512mb',
+                                    user_data=f"""#!/bin/bash
+        export IP={args.ip}
+        export EMAIL={args.email}
+        yum -y update && yum -y upgrade
+        yum -y install wget
+        wget -O /root/openvpn-deploy.sh https://raw.githubusercontent.com/LopezNathan/VPN-Deployer/master/OpenVPN-Deploy.sh
+        chmod +x /root/openvpn-deploy.sh && bash /root/openvpn-deploy.sh""",
+                                    backups=True)
+
+        droplet.create()
+
+vpn = Deploy()
+vpn.create_vpn(args.ip, args.email)
