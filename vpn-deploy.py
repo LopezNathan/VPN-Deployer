083552e080daf7158a1a0d9b72c2836bad05d423
Nathan Lopez <contact@lopeznathan.com>
2019-02-20 03:36:13 -0600

    vpn-deploy.py v0.2 - Pull DO API Token from Environment Variable

--- vpn-deploy.py
+++ vpn-deploy.py
@@ -2,11 +2,14 @@
 # VPN Deploy Script
 # Fully Install OpenVPN on DigitalOcean Automatically
 # Utilizes OpenVPN-Install by Angristan (https://github.com/Angristan/OpenVPN-install)
-# Version 0.1
+# Version 0.2
 
+import os
 import fileinput
 import argparse
 import requests
+
+DO_API_TOKEN=os.environ["DO_API_TOKEN"]
 
 parser = argparse.ArgumentParser(description="VPN Deploy Script")
 parser.add_argument("ip", help="Your IP Address")
@@ -26,7 +29,7 @@ with open('/tmp/user-data.txt', 'r') as myfile:
 myfile.close()
     
 payload = {'name': 'test', 'region': 'nyc3', 'size': '512mb', 'image': 'centos-7-x64', 'user_data': user_data}
-create = requests.post("https://api.digitalocean.com/v2/droplets", params=payload, headers={"Authorization": "Bearer DO_API","Content-Type": "application/json"})
+create = requests.post("https://api.digitalocean.com/v2/droplets", params=payload, headers={"Authorization": "Bearer %s" % DO_API_TOKEN, "Content-Type": "application/json"})
 
 for i in create:
     print(i)
