8a66cb54193bf0ea352d444fd6152625aa7b8874
Nathan Lopez <contact@lopeznathan.com>
2019-02-27 01:23:25 -0600

    vpn-deploy.py & vpn-installer.sh - Made email Argument Optional

--- vpn-deploy.py
+++ vpn-deploy.py
@@ -2,7 +2,7 @@
 # VPN Deploy Script
 # Fully Install OpenVPN on DigitalOcean Automatically
 # Utilizes OpenVPN-Install by Angristan (https://github.com/Angristan/OpenVPN-install)
-# Version 0.6.1
+# Version 0.7.0
 
 import os
 import argparse
@@ -15,14 +15,14 @@ DO_API_TOKEN=os.environ["DO_API_TOKEN"]
 
 parser = argparse.ArgumentParser(description="VPN Deploy Script with DigitalOcean")
 parser.add_argument("ip", help="Your IP Address")
-parser.add_argument("email", help="Your Email Address")
+parser.add_argument("--email", dest="email", help="Your Email Address")
 parser.add_argument("--name", default='VPN', dest="name", help="Droplet Name")
 parser.add_argument("--region", default='nyc1', dest="region", help="Droplet Region")
 args = parser.parse_args()
 
 class Deploy:
 
-    def create_vpn(self, ip_address, email_address):
+    def create_vpn(self, ip_address):
         droplet = digitalocean.Droplet(token=f'{DO_API_TOKEN}',
                                     name=f'{args.name}',
                                     region=f'{args.region}',
@@ -38,7 +38,7 @@ class Deploy:
         droplet.create()
 
 vpn = Deploy()
-vpn.create_vpn(args.ip, args.email)
+vpn.create_vpn(args.ip)
 
 time.sleep(10)
 print("Deploy Started!")
--- vpn-installer.sh
+++ vpn-installer.sh
@@ -2,7 +2,7 @@
 # Cloud VPN Deploy Script
 # Fully Install OpenVPN on DigitalOcean Automatically
 # Utilizes OpenVPN-Install by Angristan (https://github.com/Angristan/OpenVPN-install)
-# Version 1.2.0
+# Version 1.3.0
 
 # Update System
 yum -y update && yum -y upgrade
@@ -13,9 +13,21 @@ chmod +x /root/openvpn-install.sh
 export AUTO_INSTALL=y
 bash /root/openvpn-install.sh
 
-# Install NGINX & Sendmail
-yum -y install nginx sendmail
-service nginx start
+# Check if EMAIL Variable Exists
+if [ "$EMAIL" ]; then
+    # Install NGINX & Sendmail
+    yum -y install nginx sendmail
+    
+    # Send Email About File Download
+    echo "Subject: VPN Client Download
+
+    Download Link: http://"$(hostname -I | cut -d' ' -f1)"/client.ovpn" > /root/email.txt
+    sendmail $EMAIL < /root/email.txt
+else
+    # Install NGINX
+    yum -y install nginx
+    service nginx start
+fi
 
 # Restrict Traffic to Specified IP
 iptables -I INPUT \! --src $IP -m tcp -p tcp --dport 80 -j DROP
@@ -23,15 +35,13 @@ iptables -I INPUT \! --src $IP -m tcp -p tcp --dport 80 -j DROP
 # Move OpenVPN Client File to NGINX Root
 cp /root/client.ovpn /usr/share/nginx/html/
 
-# Send Email About File Download
-echo "Subject: VPN Client Download
-
-Download Link: http://"$(hostname -I | cut -d' ' -f1)"/client.ovpn" > /root/email.txt
-sendmail $EMAIL < /root/email.txt
-
 # Wait 5 Minutes for Download before Cleanup
 sleep 300
 
 # Start Cleanup
-yum -y remove nginx sendmail && yum clean all
+if [ "$EMAIL" ]; then
+    yum -y remove nginx sendmail && yum clean all
+else
+    yum -y remove nginx && yum clean all
+fi
 rm -f /usr/share/nginx/html/client.ovpn /root/vpn-installer.sh /root/openvpn-install.sh /root/client.ovpn /root/email.txt
