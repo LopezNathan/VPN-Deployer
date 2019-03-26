374c678451d7cca337b32db7028af8edf7212ca6
Nathan Lopez <contact@lopeznathan.com>
2019-03-26 06:25:18 -0500

    main.py - Deploy Action Added

--- src/vpndeployer/main.py
+++ src/vpndeployer/main.py
@@ -11,4 +11,31 @@ def create_parser():
     return parser
 
 def main():
+    import getpass
+    import time
+    import requests
+    from vpndeployer import droplets
+
+    # TODO - check api token auth before proceeding
+    DO_API_TOKEN = getpass.getpass('DigitalOcean API Token: ')
+    droplets.api_authentication(DO_API_TOKEN)
+
     args = create_parser().parse_args()
+
+    print("\nDeploy Started!")
+    print("This process typically takes less than 5 minutes.\n")
+
+    droplets.create_droplet(args.ip)
+    time.sleep(10)
+    droplets.get_droplet_ip()
+
+    # TODO - Use something else instead of a while loop to check the actual progress.
+    # can we move it into the class and check the progress on a class method easier?
+    while True:
+        try:
+            check_deploy = requests.get(f"http://{droplet_ip}/client.ovpn")
+            print(f"\nDeploy Completed!\nDownload OpenVPN File: http://{droplet_ip}/client.ovpn")
+            break
+        except:
+            print("Deploy In-Progress...")
+            time.sleep(60)
