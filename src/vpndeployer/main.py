abcd2ffb18a8d1b256fb5a2171787e398bddbf32
Nathan Lopez <contact@lopeznathan.com>
2019-03-27 13:42:36 -0500

    main.py - IP Argument Made Optional

--- src/vpndeployer/main.py
+++ src/vpndeployer/main.py
@@ -4,7 +4,7 @@ import argparse
 def create_parser():
     parser = argparse.ArgumentParser(description="VPN Deploy Script with DigitalOcean")
     
-    parser.add_argument("ip", help="Your IP Address")
+    parser.add_argument("--ip", dest="ip", help="Your IP Address")
     parser.add_argument("--email", dest="email", help="Email Address for OpenVPN download link")
     parser.add_argument("--name", default='VPN', dest="name", help="Droplet Name")
     parser.add_argument("--region", default='nyc1', dest="region", help="Droplet Region")
@@ -15,13 +15,18 @@ def main():
     import getpass
     import time
     import requests
+    import subprocess
     from vpndeployer import droplets
+
+    args = create_parser().parse_args()
 
     # TODO - check api token auth before proceeding
     DO_API_TOKEN = getpass.getpass('DigitalOcean API Token: ')
     droplets.api_authentication(DO_API_TOKEN)
 
-    args = create_parser().parse_args()
+    if args.ip is None:
+        args.ip = subprocess.check_output(['curl', '--silent', 'ifconfig.co'])
+        args.ip = args.ip.decode().strip('\n')
 
     print("\nDeploy Started!")
     print("This process typically takes less than 5 minutes.\n")
