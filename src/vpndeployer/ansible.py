6b790978ad2595bbe459b1e4340c969bdb5130e1
Nathan Lopez <contact@lopeznathan.com>
2019-04-17 17:10:25 -0500

    SSH Key Argument Implemented

--- src/vpndeployer/ansible.py
+++ src/vpndeployer/ansible.py
@@ -12,24 +12,23 @@ import os
 # If Cleanup Flag Provided, Run Cleanup Playbook on Remote Host
 ###
 
-cwd = Path(__file__).resolve().parent
-playbook_path = str(cwd) + '/playbooks'
+def playbook_path():
+    cwd = Path(__file__).resolve().parent
+    path = str(cwd) + '/playbooks'
+
+    return path
 
 def gen_sshkey():
-    runner = ansible_runner.run(private_data_dir=playbook_path, playbook='local-sshkeygen.yml', host_pattern='localhost', quiet=True)
+    data_path = playbook_path()
+    runner = ansible_runner.run(private_data_dir=data_path, playbook='local-sshkeygen.yml', host_pattern='localhost', quiet=True)
 
-    with open('/tmp/VPN-Test', 'r') as file:
+    with open('/tmp/.VPN-Deployer', 'r') as file:
         sshkey = file.read()
         file.close
 
-    with open(playbook_path + "/env/ssh_key", "w+") as file:
+    with open(data_path + "/env/ssh_key", "w+") as file:
         file.writelines(sshkey)
         os.chmod(str(file.name), 0o600)
         file.close
 
-gen_sshkey()
-
-# def testcmd():
-#     runner = ansible_runner.run(private_data_dir=playbook_path, host_pattern='droplets', module='ping')
-
-# testcmd()
+    return runner.status
